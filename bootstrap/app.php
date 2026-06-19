125acf90f913c73f10e43ad9e072e2ad53bed07c
asepjamaludinn <asepjamaludinn24@gmail.com>
2026-06-20 02:01:48 +0700

    fix: laravel bootstrap app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -18,12 +18,5 @@ $app = Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
-    })->create();
-if (isset($_SERVER['LAMBDA_TASK_ROOT'])) {
-    $tmpPath = '/tmp/storage';
-    if (!is_dir($tmpPath)) mkdir($tmpPath, 0777, true);
-    
-    $app->useStoragePath($tmpPath);
-}
-
-return $app;
+    })
+    ->create();
