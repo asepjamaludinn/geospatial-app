81644b39b905e44499954f5289c40c79879ef43f
asepjamaludinn <asepjamaludinn24@gmail.com>
2026-06-20 00:30:50 +0700

    cache config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,9 +14,9 @@ $app = Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    -->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
+            fn (Request $request) => true 
         );
     })->create();
 if (isset($_SERVER['LAMBDA_TASK_ROOT'])) {
