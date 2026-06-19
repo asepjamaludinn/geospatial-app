350843bbf1c41229a9a9c0c062043f970fc91e48
asepjamaludinn <asepjamaludinn24@gmail.com>
2026-06-20 00:25:45 +0700

    vercel

--- public/index.php
+++ public/index.php
@@ -8,7 +8,20 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
+
 require __DIR__.'/../vendor/autoload.php';
+    
+$appStorage = '/tmp/storage';
+if (!file_exists($appStorage)) {
+    mkdir($appStorage, 0777, true);
+    mkdir($appStorage.'/framework/views', 0777, true);
+    mkdir($appStorage.'/framework/cache', 0777, true);
+    mkdir($appStorage.'/framework/cache/data', 0777, true);
+    mkdir($appStorage.'/framework/sessions', 0777, true);
+    mkdir($appStorage.'/logs', 0777, true);
+}
+$_SERVER['APP_STORAGE'] = $appStorage;
+
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
